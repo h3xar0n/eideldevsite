1d535f5b06cfe560c7707d72e3821062bc7113e4
Aron Eidelman <aron@eideldev.com>
2017-04-07 15:33:51 -0400

    JavaScript Lite Brite project

---
+++ projects/js-lite-brite/js/main.js
@@ -0,0 +1,41 @@
+var main = function() {
+  
+  var colorClass = '';
+  
+  $('.select-color').on('click', 
+    function() {
+      var selectedColor = $(this).attr('class');
+    
+      switch (selectedColor) {
+        case 'select-color magenta not-selected':
+          colorClass = 'magenta';
+        break;
+        case 'select-color yellow not-selected':
+          colorClass = 'yellow';
+        break;
+        case 'select-color cyan not-selected':
+          colorClass = 'cyan';
+        break;
+      }
+    
+      $(this).removeClass('not-selected');
+      $(this).siblings().addClass('not-selected');
+  });
+  
+  $('.box').on('click',
+    function() {
+      $(this).toggleClass(colorClass);
+  });
+
+  $('.toggle-blink').on('click', 
+    function() {
+      if (colorClass) {
+        $('.toggle-blink').toggleClass('opacity');
+      }
+      setInterval(function() {
+        $('.box.cyan, .box.yellow, .box.magenta').toggleClass('blink');
+      }, 350);
+  });
+}
+
+$(document).ready(main);
