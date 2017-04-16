1fdde5062b4a38788e454553794412926a569756
Aron Eidelman <aron@eideldev.com>
2017-04-16 13:13:04 -0400

    Angular Gameboard, combined with jQuery

---
+++ projects/angular_gameboard/js/jquery.js
@@ -0,0 +1,13 @@
+var main = function() {
+  $('.more-btn').click(function(){
+    $(this).next().toggle();
+  });
+  $('.share').click(function(){
+    $(this).next().toggle();
+  });
+  $('.notification').click(function(){
+    $(this).toggleClass('active');
+  });
+};
+
+$(document).ready(main);
