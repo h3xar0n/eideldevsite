31f22cce38a35084724fed9ae23682e8df55d718
h3xar0n <aron@glambyadelina.com>
2017-04-29 16:30:27 -0400

    defining routes

--- projects/angular_calendar/js/app.js
+++ projects/angular_calendar/js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('CalendarApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    controller: "DayController",
+    templateUrl: "views/day.html"
+  })
+  .otherwise({
+    redirectTo: "/"
+  });
+});
