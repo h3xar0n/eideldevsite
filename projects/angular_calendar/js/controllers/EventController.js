9277456d5a3f948dc8d48c0c24954c908bdab6cc
h3xar0n <aron@glambyadelina.com>
2017-04-29 18:36:55 -0400

    Events controller and views

--- projects/angular_calendar/js/controllers/EventController.js
+++ projects/angular_calendar/js/controllers/EventController.js
@@ -1,3 +1,5 @@
-app.controller('EventController', ['$scope', '$routeParams', function($scope, $routeParams) {
-  
+app.controller('EventController', ['$scope', 'events', '$routeParams', function($scope, events, $routeParams) {
+  events.success(function(data) {
+    $scope.event = data.events[$routeParams.id];
+  });
 }]);
