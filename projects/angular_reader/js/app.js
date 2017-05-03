536b96fcbf473327c6a3ec817273c1c620c24f1f
h3xar0n <aron@glambyadelina.com>
2017-05-03 19:31:30 -0400

    Defined app's routes for Bookshelf

--- projects/angular_reader/js/app.js
+++ projects/angular_reader/js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('ReaderApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/books", {
+    controller: "BookshelfController",
+    templateUrl: "views/bookshelf.html"
+  })
+  .otherwise({
+    redirectTo: "/"
+  });
+});
