bab623c948954e317c8002f1051e5f709a81cf10
h3xar0n <aron@glambyadelina.com>
2017-05-23 12:56:43 -0400

    Arranged all controllers and views to show list of all books, details of a specific book, and selected chapters from the book

--- projects/angular_reader/js/controllers/BookshelfController.js
+++ projects/angular_reader/js/controllers/BookshelfController.js
@@ -1,3 +1,5 @@
-app.controller('BookshelfController', ['$scope', function($scope) {
-
+app.controller('BookshelfController', ['$scope', 'books',  function($scope, books) {
+  books.success(function(data) {
+    $scope.myBooks = data;
+  });
 }]);
--- projects/angular_reader/js/controllers/ChapterController.js
+++ projects/angular_reader/js/controllers/ChapterController.js
@@ -1,5 +1,7 @@
 app.controller('ChapterController', ['$scope', 'books', '$routeParams', function($scope, books, $routeParams) {
   books.success(function(data) {
+    $scope.book = data[$routeParams.book];
+    $scope.chapter = $scope.book.chapters[$routeParams.chapterId];
 
     // If there no more chapters left, go back to the bookshelf view
     if($routeParams.chapterId >= $scope.book.chapters.length - 1) {
@@ -8,7 +10,7 @@ app.controller('ChapterController', ['$scope', 'books', '$routeParams', function
   });
 
   // Using these properties to create the URLs in line 1 and line 11 of view/chapter.html
-  $scope.currentBookIndex = parseInt($routeParams.bookId);
+  $scope.currentBookIndex = parseInt($routeParams.book);
   $scope.currentChapterIndex = parseInt($routeParams.chapterId);
   $scope.nextChapterIndex = $scope.currentChapterIndex + 1;
 }]);
