a4455d5f87a6c44615d0dfc6eed6268ec6f595c0
Aron Eidelman <aroneidelman@Aytajs-MacBook-Pro.local>
2017-06-30 18:35:56 -0400

    Set up initial particle canvas

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,118 @@
+// Particles
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 380,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
 // Vue
 
 // Work Samples
-
 var workSampleList = new Vue({
   el: '#work-samples',
   data: {
@@ -45,7 +156,6 @@ var workSampleList = new Vue({
     ]
   }
 })
-
 var skillList = new Vue({
   el: '#skill-list',
   data: {
@@ -73,7 +183,6 @@ var skillList = new Vue({
 })
 
 $(document).ready(function(){
-
   // Smooth Scrolling
   var $root = $('html, body');
   $('.navbar-nav a, .workName a, .btm-but a').click(function() {
@@ -85,15 +194,12 @@ $(document).ready(function(){
     });
     return false;
   });
-
   // Navbar Drop Down
   $(".navbar-nav li a").click(function(event) {
     if (!$(this).parent().hasClass('dropdown'))
     $(".navbar-collapse").collapse('hide');
   });
-
   // Work: Portfolio Effect
-
   $('#skills-used i').mouseenter(function() {
     var skill = $(this).attr('class')
     if (skill == 'devicon-ruby-plain colored') {
@@ -127,7 +233,6 @@ $(document).ready(function(){
     $('.portfolio-sample').removeClass('select-sample');
     $('.sample-title').html('Work');
   });
-
   // History Image Slide-In
   $(window).scroll(function(){
     $('.container p').each(function(){
@@ -136,22 +241,18 @@ $(document).ready(function(){
             distance      = (elementOffset - scrollTop),
             windowHeight  = $(window).height(),
             breakPoint    = windowHeight*0.9;
-
-        if(distance > breakPoint) {
+        if (distance > breakPoint) {
           $(this).addClass("more-padding");
         }  if(distance < breakPoint) {
           $(this).removeClass("more-padding");
         }
     });
   });
-
   //Parallax
   $('.devaron-background').parallax({imageSrc: 'img/Grid2.jpg'});
-
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
-
   //Skills
   var skillsIndex = 0;
   while (skillsIndex < skills.length) {
@@ -164,7 +265,6 @@ $(document).ready(function(){
     icon.className = skills[skillsIndex].icon + skills[skillsIndex].color;
     skillsIndex ++;
   }
-
   //Contact Form
   $("#input-message").css({
     "background-color": "white",
